fdd20fa5373509568b4cb784c4dadb5a94027ab6
adamj <amrachek@outlook.com>
2019-03-19 20:02:47 -0600

    First version of RPS Lizard Spock

---
+++ lesson_2/rps/rock_paper_scissors.rb
@@ -0,0 +1,187 @@
+class Move
+  VALUES = ['rock', 'paper', 'scissors', 'lizard', 'spock']
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def lizard?
+    @value == 'lizard'
+  end
+
+  def spock?
+    @value == 'spock'
+  end
+
+  def >(other_move)
+    (rock? && (other_move.scissors? || other_move.lizard?)) ||
+      (paper? && (other_move.rock? || other_move.spock?)) ||
+      (scissors? && (other_move.paper? || other_move.lizard?)) ||
+      (lizard? && (other_move.spock? || other_move.paper?)) ||
+      (spock? && (other_move.rock? || other_move.scissors?))
+  end
+
+  def <(other_move)
+    (rock? && (other_move.paper? || other_move.spock?)) ||
+      (paper? && (other_move.scissors? || other_move.lizard?)) ||
+      (scissors? && (other_move.rock? || other_move.spock?)) ||
+      (lizard? && (other_move.rock? || other_move.scissors?)) ||
+      (spock? && (other_move.paper? || other_move.lizard?))
+  end
+
+  def to_s
+    @value
+  end
+end
+
+class Player
+  attr_accessor :move, :name, :score
+
+  def initialize
+    set_name
+    @score = 0
+  end
+end
+
+class Human < Player
+  def set_name
+    n = ""
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+
+      puts "Sorry, you must enter a value."
+    end
+    self.name = n
+  end
+
+  def choose
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, scissors, lizard, or spock."
+      choice = gets.chomp
+      break if Move::VALUES.include?(choice)
+
+      puts "Sorry, invalid choice"
+    end
+    self.move = Move.new(choice)
+  end
+end
+
+class Computer < Player
+  def set_name
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+  end
+
+  def choose
+    self.move = Move.new(Move::VALUES.sample)
+  end
+end
+
+class RPSGame
+  attr_accessor :human, :computer
+
+  def initialize
+    @human = Human.new
+    @computer = Computer.new
+  end
+
+  def display_welcome_message
+    puts "Hi, #{human.name}. Welcome to Rock, Paper, Scissors, Lizard, Spock!"
+  end
+
+  def display_goodbye_message
+    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Goodbye!"
+  end
+
+  def display_moves
+    puts "#{human.name} chose #{human.move}."
+    puts "#{computer.name} chose #{computer.move}."
+  end
+
+  def display_round_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def update_score
+    if human.move > computer.move
+      human.score += 1
+    elsif human.move < computer.move
+      computer.score += 1
+    end
+  end
+
+  def display_score
+    puts "#{human.name}: #{human.score}"
+    puts "#{computer.name}: #{computer.score}"
+  end
+
+  def game_winner?
+    human.score == 10 || computer.score == 10
+  end
+
+  def display_game_winner
+    if human.score == 10
+      puts "#{human.name} wins the game!"
+    elsif computer.score == 10
+      puts "#{computer.name} wins the game!"
+    end
+  end
+
+  def play_again?
+    choice = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      choice = gets.chomp.downcase
+      break if ['y', 'n'].include?(choice)
+
+      puts "Sorry, must be y or n."
+    end
+    choice == 'y'
+  end
+
+  def reset_score
+    human.score = 0
+    computer.score = 0
+  end
+
+  def play
+    display_welcome_message
+    loop do
+      reset_score
+
+      loop do
+        human.choose
+        computer.choose
+        display_moves
+        display_round_winner
+        update_score
+        display_score
+        break if game_winner?
+      end
+      display_game_winner
+      break unless play_again?
+    end
+    display_goodbye_message
+  end
+end
+
+RPSGame.new.play
