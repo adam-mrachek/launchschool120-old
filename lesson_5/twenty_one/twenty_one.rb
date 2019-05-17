258e25a8dd75c5d2ae09d3fe7312575475c16e9b
adamj <amrachek@outlook.com>
2019-05-17 13:20:06 -0600

    Added #play_again? and did some refactoring.

--- lesson_5/twenty_one/twenty_one.rb
+++ lesson_5/twenty_one/twenty_one.rb
@@ -1,17 +1,32 @@
 require 'pry'
+module Utilities
+  def empty_line
+    puts ""
+  end
+
+  def clear
+    system('clear') || system('cls')
+  end
+
+  def horizontal_rule
+    puts "---------------------"
+  end
+end
 
 class Player
   attr_accessor :hand
 
   def initialize(name)
-    # what would the 'data' or 'states' of a Player object entail?
-    # maybe cards? name?
     @hand = []
     @name = name
   end
 
   def show_hand
-    puts "#{@name} has #{card_values(@hand)} for a total of #{total}."
+    puts "#{@name}'s hand: #{card_values(@hand)} for a total of #{total}."
+  end
+
+  def show_face_up_card
+    puts "Dealer is showing #{@hand[0][1]}."
   end
 
   def hit(deck)
@@ -34,6 +49,7 @@ class Player
     total > winning_score
   end
 
+  # rubocop: disable Style/ConditionalAssignment
   def card_values(hand)
     cards = ""
     hand.each do |card|
@@ -45,7 +61,9 @@ class Player
     end
     cards
   end
+  # rubocop: enable Style/ConditionalAssignment
 
+  # rubocop: disable Style/ConditionalAssignment
   def total
     sum = 0
     values = @hand.map { |card| card[1] }
@@ -65,6 +83,7 @@ class Player
 
     sum
   end
+  # rubocop: enable Style/ConditionalAssignment
 end
 
 class Deck
@@ -93,29 +112,45 @@ class Deck
   end
 end
 
-class Card
-  def initialize
-    # what are the 'states' of a card?
-  end
-end
-
 class Game
+  include Utilities
+
   WINNING_SCORE = 21
   DEALER_STAY = 17
 
   def initialize
+    shuffle_deck_clear_hands
+  end
+
+  def shuffle_deck_clear_hands
     @game_deck = Deck.new
     @player = Player.new('Adam')
     @dealer = Player.new('Dealer')
   end
 
   def start
-    deal_cards
-    show_initial_cards
-    player_turn
-    dealer_turn unless @player.busted?(WINNING_SCORE)
-    show_final_cards
-    show_result
+    welcome
+    loop do
+      deal_cards
+      show_initial_cards
+      player_turn
+      dealer_turn unless @player.busted?(WINNING_SCORE)
+      sleep 1.0
+      show_result
+      show_final_cards
+      break unless play_again?
+
+      shuffle_deck_clear_hands
+      clear
+    end
+  end
+
+  def welcome
+    clear
+    puts "Welcome to 21!"
+    puts "The player with the highest score "\
+         "without going over #{WINNING_SCORE} wins!"
+    horizontal_rule
   end
 
   def deal_cards
@@ -127,16 +162,13 @@ class Game
 
   def show_initial_cards
     @player.show_hand
-    show_dealer_face_up_card
+    @dealer.show_face_up_card
   end
 
   def show_final_cards
     @player.show_hand
     @dealer.show_hand
-  end
-
-  def show_dealer_face_up_card
-    puts "Dealer is showing #{@dealer.hand[0][1]}."
+    horizontal_rule
   end
 
   def player_turn
@@ -149,14 +181,20 @@ class Game
   end
 
   def dealer_turn
+    @dealer.show_hand
     while @dealer.total < DEALER_STAY
+      sleep 1.0
+      empty_line
+      puts "**Dealer hits**"
+      empty_line
       @dealer.hit(@game_deck)
-      @dealer.show_hand
       sleep 1.5
+      @dealer.show_hand
     end
   end
 
   def show_result
+    horizontal_rule
     if @player.busted?(WINNING_SCORE)
       puts "You busted! Dealer wins!"
     elsif @dealer.busted?(WINNING_SCORE)
@@ -170,6 +208,17 @@ class Game
     end
   end
 
+  def play_again?
+    answer = ""
+    loop do
+      puts "Would you like to play again? (y or n)"
+      answer = gets.chomp.downcase
+      break if %w(y n).include?(answer)
+
+      puts "Sorry, invalid choice."
+    end
+    answer == 'y'
+  end
 end
 
 Game.new.start
